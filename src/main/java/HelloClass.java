65ca3a19ce2f3dc9fb0594cb203d930becd52f16
HarshadaDawande <HarshadaDawande@gmail.com>
2021-11-10 16:55:08 +0530

    Added new sysout

--- src/main/java/HelloClass.java
+++ src/main/java/HelloClass.java
@@ -1,5 +1,6 @@
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("First output line on this Mac");
+        System.out.println("This sout is added for branch 1");
     }
 }
